0abd7bfe9c80e044031b9fe6bc9b52e9b5c2b3f0
maxrgnt <maxrgnt@umich.edu>
2019-10-03 08:55:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -79,11 +79,10 @@ def radio_results(image_you_chose):
     return html.Img(src=app.get_asset_url(image_you_chose), style={'width': 'auto', 'height': '50%'})
 
 @app.callback(Output('figure-1', 'figure'),
-              [Input('your_input_here', 'value')],
-              [Input("dfalse", "value")]
+              [Input('your_input_here', 'value')]
              )
-def new_fig(image_you_chose, val):
-    return create_fig(val)
+def new_fig(image_you_chose):
+    return create_fig(5)
 
 # @app.callback(
 #     Output("figure-1", "figure"),
