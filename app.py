1fcb1c4fdd93855089370e2dccb9d7733c71576b
maxrgnt <maxrgnt@umich.edu>
2019-10-03 07:39:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def radio_results(image_you_chose):
 @app.callback(Output('figure-1', 'figure'),
               [Input('your_input_here', 'value')]
              )
-def new_fig():
+def new_fig(image_you_chose):
     return create_fig()
 
 ############ Deploy
